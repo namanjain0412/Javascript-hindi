efbc865c5262f94c0aa44c45fd214a1f7a0e24b6
Naman Jain <140949997+namanjain0412@users.noreply.github.com>
2024-12-03 11:47:45 +0000

    datatypes

---
+++ .devcontainer/01_basics/datatype.js
@@ -0,0 +1,20 @@
+
+console.log(typeof "namanjain") //string
+console.log(typeof null) //object ***
+console.log(typeof undefined) //undefined
+function greet() {
+    console.log("Hello!");
+  }
+  let colors = ["red", "green", "blue"];
+  console.log(colors);
+  let person = {
+    name: "Alice",
+    age: 30
+  };
+  console.log(person);
+  let uniqueID = Symbol('id');
+  console.log(uniqueID);
+  let isLoggedIn = true;
+  let isOver18 = false;
+  console.log(typeof true); // "boolean"
+  
